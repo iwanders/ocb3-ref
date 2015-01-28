873cb577e2343265125b1bf9cabff90561e10644
Ivor Wanders <ivor@iwanders.net>
2015-01-28 16:10:32 +0100

    Introduces the header file for OCB3.
    Moves relevant parts from the c file to the header file.

--- ocb.c
+++ ocb.c
@@ -38,41 +38,13 @@
 /    when compiling under MSVC. If untrue, alter the #define.
 /  - This code is tested for C99 and recent versions of GCC and MSVC.      */
 
-/* ----------------------------------------------------------------------- */
-/* User configuration options                                              */
-/* ----------------------------------------------------------------------- */
-
-/* Set the AES key length to use and length of authentication tag to produce.
-/  Setting either to 0 requires the value be set at runtime via ae_init().
-/  Some optimizations occur for each when set to a fixed value.            */
-#define OCB_KEY_LEN         16  /* 0, 16, 24 or 32. 0 means set in ae_init */
-#define OCB_TAG_LEN         16  /* 0 to 16. 0 means set in ae_init         */
-
-/* This implementation has built-in support for multiple AES APIs. Set any
-/  one of the following to non-zero to specify which to use.               */
-#define USE_OPENSSL_AES      0  /* http://openssl.org                      */
-#define USE_REFERENCE_AES    0  /* Internet search: rijndael-alg-fst.c     */
-#define USE_AES_NI           1  /* Uses compiler's intrinsics              */
-
-/* During encryption and decryption, various "L values" are required.
-/  The L values can be precomputed during initialization (requiring extra
-/  space in ae_ctx), generated as needed (slightly slowing encryption and
-/  decryption), or some combination of the two. L_TABLE_SZ specifies how many
-/  L values to precompute. L_TABLE_SZ must be at least 3. L_TABLE_SZ*16 bytes
-/  are used for L values in ae_ctx. Plaintext and ciphertexts shorter than
-/  2^L_TABLE_SZ blocks need no L values calculated dynamically.            */
-#define L_TABLE_SZ          16
-
-/* Set L_TABLE_SZ_IS_ENOUGH non-zero iff you know that all plaintexts
-/  will be shorter than 2^(L_TABLE_SZ+4) bytes in length. This results
-/  in better performance.                                                  */
-#define L_TABLE_SZ_IS_ENOUGH 1
 
 /* ----------------------------------------------------------------------- */
 /* Includes and compiler specific definitions                              */
 /* ----------------------------------------------------------------------- */
 
 #include "ae.h"
+#include "ocb3-ref.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -140,7 +112,7 @@
 #if __SSE2__
     #include <xmmintrin.h>              /* SSE instructions and _mm_malloc */
     #include <emmintrin.h>              /* SSE2 instructions               */
-    typedef __m128i block;
+    // typedef __m128i block;
     #define xor_block(x,y)        _mm_xor_si128(x,y)
     #define zero_block()          _mm_setzero_si128()
     #define unequal_blocks(x,y) \
@@ -179,7 +151,7 @@
 	}
 #elif __ALTIVEC__
     #include <altivec.h>
-    typedef vector unsigned block;
+    // typedef vector unsigned block;
     #define xor_block(x,y)         vec_xor(x,y)
     #define zero_block()           vec_splat_u32(0)
     #define unequal_blocks(x,y)    vec_any_ne(x,y)
@@ -229,7 +201,7 @@
 	}
 #elif __ARM_NEON__
     #include <arm_neon.h>
-    typedef int8x16_t block;      /* Yay! Endian-neutral reads! */
+    // typedef int8x16_t block;      /* Yay! Endian-neutral reads! */
     #define xor_block(x,y)             veorq_s8(x,y)
     #define zero_block()               vdupq_n_s8(0)
     static inline int unequal_blocks(block a, block b) {
@@ -260,7 +232,7 @@
 		return veorq_s8(tmp,b);
 	}
 #else
-    typedef struct { uint64_t l,r; } block;
+    // typedef struct { uint64_t l,r; } block;
     static inline block xor_block(block x, block y) {
     	x.l^=y.l; x.r^=y.r; return x;
     }
@@ -345,14 +317,14 @@ static inline void AES_ecb_decrypt_blks(block *blks, unsigned nblks, AES_KEY *ke
 
 #include "rijndael-alg-fst.h"              /* Barreto's Public-Domain Code */
 #if (OCB_KEY_LEN == 0)
-	typedef struct { uint32_t rd_key[60]; int rounds; } AES_KEY;
+	// typedef struct { uint32_t rd_key[60]; int rounds; } AES_KEY;
 	#define ROUNDS(ctx) ((ctx)->rounds)
 	#define AES_set_encrypt_key(x, y, z) \
 	 do {rijndaelKeySetupEnc((z)->rd_key, x, y); (z)->rounds = y/32+6;} while (0)
 	#define AES_set_decrypt_key(x, y, z) \
 	 do {rijndaelKeySetupDec((z)->rd_key, x, y); (z)->rounds = y/32+6;} while (0)
 #else
-	typedef struct { uint32_t rd_key[OCB_KEY_LEN+28]; } AES_KEY;
+	// typedef struct { uint32_t rd_key[OCB_KEY_LEN+28]; } AES_KEY;
 	#define ROUNDS(ctx) (6+OCB_KEY_LEN/4)
 	#define AES_set_encrypt_key(x, y, z) rijndaelKeySetupEnc((z)->rd_key, x, y)
 	#define AES_set_decrypt_key(x, y, z) rijndaelKeySetupDec((z)->rd_key, x, y)
@@ -383,10 +355,10 @@ static void AES_ecb_encrypt_blks(block *blks, unsigned nblks, AES_KEY *key) {
 #include <wmmintrin.h>
 
 #if (OCB_KEY_LEN == 0)
-	typedef struct { __m128i rd_key[15]; int rounds; } AES_KEY;
+	// typedef struct { __m128i rd_key[15]; int rounds; } AES_KEY;
 	#define ROUNDS(ctx) ((ctx)->rounds)
 #else
-	typedef struct { __m128i rd_key[7+OCB_KEY_LEN/4]; } AES_KEY;
+	// typedef struct { __m128i rd_key[7+OCB_KEY_LEN/4]; } AES_KEY;
 	#define ROUNDS(ctx) (6+OCB_KEY_LEN/4)
 #endif
 
@@ -568,24 +540,24 @@ static inline void AES_ecb_decrypt_blks(block *blks, unsigned nblks, AES_KEY *ke
 / with the plaintext or ciphertext.
 /------------------------------------------------------------------------- */
 
-struct _ae_ctx {
-    block offset;                          /* Memory correct               */
-    block checksum;                        /* Memory correct               */
-    block Lstar;                           /* Memory correct               */
-    block Ldollar;                         /* Memory correct               */
-    block L[L_TABLE_SZ];                   /* Memory correct               */
-    block ad_checksum;                     /* Memory correct               */
-    block ad_offset;                       /* Memory correct               */
-    block cached_Top;                      /* Memory correct               */
-	uint64_t KtopStr[3];                   /* Register correct, each item  */
-    uint32_t ad_blocks_processed;
-    uint32_t blocks_processed;
-    AES_KEY decrypt_key;
-    AES_KEY encrypt_key;
-    #if (OCB_TAG_LEN == 0)
-    unsigned tag_len;
-    #endif
-};
+// struct _ae_ctx {
+    // block offset;                          /* Memory correct               */
+    // block checksum;                        /* Memory correct               */
+    // block Lstar;                           /* Memory correct               */
+    // block Ldollar;                         /* Memory correct               */
+    // block L[L_TABLE_SZ];                   /* Memory correct               */
+    // block ad_checksum;                     /* Memory correct               */
+    // block ad_offset;                       /* Memory correct               */
+    // block cached_Top;                      /* Memory correct               */
+	// uint64_t KtopStr[3];                   /* Register correct, each item  */
+    // uint32_t ad_blocks_processed;
+    // uint32_t blocks_processed;
+    // AES_KEY decrypt_key;
+    // AES_KEY encrypt_key;
+    // #if (OCB_TAG_LEN == 0)
+    // unsigned tag_len;
+    // #endif
+// };
 
 /* ----------------------------------------------------------------------- */
 /* L table lookup (or on-the-fly generation)                               */
